baaadc104265e48d3e7eedff05e3c4af7521624f
Hojjat <h.galeh@gmail.com>
2020-10-19 21:18:34 +0330

    reverse to kth element

--- test/queue.spec.ts
+++ test/queue.spec.ts
@@ -30,7 +30,7 @@ class Queue implements IQueue {
 
 }
 
-function reverse(q: IQueue) {
+function reverse(q: IQueue): IQueue {
     const arr: number[] = [];
     while(!q.isEmpty()) {
         arr.push(q.remove());
@@ -42,10 +42,36 @@ function reverse(q: IQueue) {
     return newq;
 }
 
+function reverseFirstK(q: IQueue, k: number): IQueue {
+    const q2 = new Queue([]);
+    const stk: number[] = [];
+    let counter = 0;
+    while(!q.isEmpty()) {
+        counter++;
+        stk.push(q.remove());
+        if (counter === k) {
+            break;
+        }
+    }
+    while(stk.length > 0) {
+        q2.add(stk.pop());
+    }
+    while(!q.isEmpty()) {
+        q2.add(q.remove());
+    }
+    return q2;
+}
+
 describe('test queue', () => {
     it('reverse queue', () => {
         const q1 = new Queue([2, 4, 5, 3, 8]);
         const rev = reverse(q1);
         expect(rev.toArray()).toEqual([8, 3, 5, 4, 2]);
+    });
+
+    it('reverse to kth element', () => {
+        const q1 = new Queue([2, 4, 5, 3, 8]);
+        const q2 = reverseFirstK(q1, 3);
+        expect(q2.toArray()).toEqual([5, 4, 2, 3, 8]);
     });
 });
